360c7404904d9d81ebf469e19b3acd5cbf3ef8f9
Bhagatsakshi <142607822+Bhagatsakshi@users.noreply.github.com>
2025-11-12 20:11:44 +0530

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,17 @@
+import LikeButton from "./LikeButton";
+import Todo from "./ToDolist";
+import LotteryGame from "./LotteryGame";
+import GetJokes from "./GetJokes";
+
+function App() {
+  return (
+    <>  
+      <LikeButton />
+      <Todo />
+      <GetJokes />
+      <LotteryGame/>
+    </>
+  );
+}
+
+export default App
